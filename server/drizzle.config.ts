ff6fd27754a824cedef92dd6bba27d2dded70ee1
Gabriel Souza <apgrl123@gmail.com>
2026-04-16 17:25:03 -0300

    trigger]

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -5,7 +5,7 @@ export default {
   dbCredentials: {
     url: env.DATABASE_URL,
   },
+  schema: "src/infra/db/schemas/*",
   dialect: "postgresql",
   out: "src/infra/db/migrations",
-  schema: "src/infra/db/schemas/*",
 } satisfies Config;
